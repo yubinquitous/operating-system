7e7af4db16598492ad43e493c0a11b9b66308692
yubinquitous <yb9854@naver.com>
2022-09-06 19:28:35 +0900

    [FIX] hcat 예외처리

--- hcat.c
+++ hcat.c
@@ -7,7 +7,7 @@ int nlines;
 
 void hcat(int fd)
 {
-  int n;
+  int n = 0;
   int cnt = 0;
 
   while ((cnt < nlines) && ((n = read(fd, &c, 1)) > 0))
@@ -31,31 +31,23 @@ int main(int argc, char *argv[])
 {
   int fd;
 
-  if (argc != 3 || !argv[1])
+  if ((argc != 2 && argc != 3) || !argv[1])
   {
     printf(1, "usage: hcat line file_name\n");
     exit();
   }
 
   nlines = atoi(argv[1]);
-  if ((fd = open(argv[2], 0)) < 0) // O_RDONLY가 0임
+  if (!argv[2])
+    fd = 0;
+  else if ((fd = open(argv[2], 0)) < 0) // O_RDONLY가 0임
   {
     printf(1, "hcat: cannot open %s\n", argv[2]);
     exit();
   }
 
   hcat(fd);
-  close(fd);
-
-  //   for (i = 1; i < argc; i++)
-  //   {
-  // 	if ((fd = open(argv[i], 0)) < 0)
-  // 	{
-  // 	  printf(1, "hcat: cannot open %s\n", argv[i]);
-  // 	  exit();
-  // 	}
-  // 	hcat(fd);
-  // 	close(fd);
-  //   }
+  if (fd)
+    close(fd);
   exit();
 }
