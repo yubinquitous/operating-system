81662cc679d2313ee143e7102f9cd0d375a1fc26
yubinquitous <yb9854@naver.com>
2022-09-20 20:24:22 +0900

    [CHORE] 줄정렬 및 테스트 코드 추가

--- proc.h
+++ proc.h
@@ -60,7 +60,7 @@ struct proc
   struct file *ofile[NOFILE]; // Open files
   struct inode *cwd;          // Current directory
   char name[16];              // Process name (debugging)
-  int trace_mask;
+  int trace_mask;             // Trace mask to control tracing
 };
 
 // Process memory is laid out contiguously, low addresses first:
--- ssu_trace.c
+++ ssu_trace.c
@@ -3,31 +3,31 @@
 int main(int argc, char *argv[])
 {
 	int pid, wpid;
-    int mask;
+	int mask;
 
-    if (argc < 2)
-    {
-        printf(2, "Usage: %s <mask> <command>\n", argv[0]);
-        exit();
-    }
+	if (argc < 2)
+	{
+		printf(2, "Usage: %s <mask> <command>\n", argv[0]);
+		exit();
+	}
 
-    if ((mask = atoi(argv[1])) < 0 || mask > 0x7FFFFFFF)
-    {
-        printf(2, "Invalid mask\n");
-        exit();
-    }
-    trace(mask);
+	if ((mask = atoi(argv[1])) < 0)
+	{
+		printf(2, "Invalid mask\n");
+		exit();
+	}
+	trace(mask);
 
 	pid = fork();
 	if (pid < 0)
 	{
-		printf(2, "init: fork failed\n");
+		printf(2, "ssu_trace: fork failed\n");
 		exit();
 	}
 	if (pid == 0)
 	{
 		exec(argv[2], argv + 2);
-		printf(2, "init: exec sh failed\n");
+		printf(2, "ssu_trace: exec failed\n");
 		exit();
 	}
 	while ((wpid = wait()) >= 0 && wpid != pid)
