b7d694a6c2768670643898c46a66724ef6301a5d
yubinquitous <yb9854@naver.com>
2022-11-20 13:52:23 +0900

    [FIX] 참조 스트링에 RW 비트 추가

--- project4/make_reference.c
+++ project4/make_reference.c
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(void)
 {
 	int page, fd;
+	char rb_status;
+	char buf[20] = {0};
 
 	fd = open("./reference.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	printf("fd : %d\n", fd);
 	if (fd < 0)
 	{
 		printf("파일 오픈 실패\n");
@@ -19,7 +21,9 @@ int main(void)
 	for (int i = 0; i < 500; i++)
 	{
 		page = rand() % 30 + 1;
-		write(fd, &page, sizeof(int));
+		rb_status = rand() % 2 ? 'R' : 'W';
+		sprintf(buf, "%d(%c) ", page, rb_status);
+		write(fd, buf, strlen(buf));
 	}
 	close(fd);
 	return 0;
