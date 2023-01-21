1fc0b1b12983ed15d4d4fe4796e73983112351c1
yubinquitous <yb9854@naver.com>
2023-01-21 15:30:20 +0900

    [FIX] esc

--- project4/simulate/esc.c
+++ project4/simulate/esc.c
@@ -1,95 +1,86 @@
 #include "../page_replacement.h"
 
-void simulate_esc(int n_frames, t_reference *reference, int fd)
-{
-	t_frame_with_rw_bit frame[n_frames]; // 프레임 구조체 배열
-	int page_fault = 0;					 // page fault 횟수
-	char is_hit;						 // hit 여부
-	int idx = 0;						 // 교체될 프레임을 가리키는 인덱스
-	char find_victim = 0;				 // victim을 찾았는지 확인하는 변수
+void simulate_esc(int n_frames, t_reference *reference, int fd) {
+  t_frame_with_rw_bit frame[n_frames]; // 프레임 구조체 배열
+  int page_fault = 0;                  // page fault 횟수
+  char is_hit;                         // hit 여부
+  int idx = 0;          // 교체될 프레임을 가리키는 인덱스
+  char find_victim = 0; // victim을 찾았는지 확인하는 변수
 
-	// frame 초기화
-	for (int i = 0; i < n_frames; i++)
-	{
-		frame[i].page = 0;
-		frame[i].r_bit = 0;
-		frame[i].w_bit = 0;
-	}
-	print_algorithm_start("ESC", n_frames, fd); // 알고리즘 정보 출력
-	for (int i = 0; i < REFERENCE_SIZE; i++)
-	{
-		find_victim = 0;
-		is_hit = 0;
-		int j = 0;
-		for (j = 0; j < n_frames; j++)
-		{
-			if (frame[j].page == 0)
-				break;
-			if (frame[j].page == reference[i].page) // hit!
-			{
-				is_hit = 1;
-				frame[j].r_bit = 1;								 // r_bit을 1로 설정
-				frame[j].w_bit = (reference[i].rw_bit == W_BIT);
-				break;
-			}
-		}
-		if (is_hit) // hit!
-		{
-			print_frame_with_rw_bit(reference[i], frame, n_frames, "HIT!", fd); // frame 출력
-			continue;															// 다음 참조값으로 넘어감
-		}
-		++page_fault;				// page fault 발생
-		if (page_fault <= n_frames) // 비어있는 프레임이 있으면
-		{
-			frame[j].page = reference[i].page;
-			frame[j].r_bit = 1;
-			frame[j].w_bit = (reference[i].rw_bit == W_BIT);
-		}
-		else // frame이 가득 찬 경우
-		{
-			while (1)
-			{
-				// 단계 1: (0, 0) 찾기
-				for (j = 0; j < n_frames; j++)
-				{
-					if (frame[idx].r_bit == 0 && frame[idx].w_bit == 0)
-					{
-						find_victim = 1;
-						break;
-					}
-					idx = (idx + 1) % n_frames; // 포인터를 다음 프레임으로 이동
-				}
-				// (0, 0)을 찾았다면 교체 후 반복문 탈출
-				if (find_victim)
-				{
-					frame[idx].page = reference[i].page;
-					frame[idx].r_bit = 1;
-					frame[idx].w_bit = (reference[i].rw_bit == W_BIT);
-					break;
-				}
-				// 단계 2: (0, 1) 찾기
-				for (j = 0; j < n_frames; j++)
-				{
-					if (frame[idx].r_bit == 0 && frame[idx].w_bit == 1)
-					{
-						find_victim = 1;
-						break;
-					}
-					idx = (idx + 1) % n_frames;
-					frame[j].r_bit = 0; // 지나쳐가는 모든 프레임의 사용 비트를 0으로 설정
-				}
-				// (0, 1)을 찾았다면
-				if (find_victim)
-				{
-					frame[idx].page = reference[i].page;
-					frame[idx].r_bit = 1;
-					frame[idx].w_bit = (reference[i].rw_bit == W_BIT);
-					break;
-				}
-			}
-			idx = (idx + 1) % n_frames; // 포인터를 다음 프레임으로 이동
-		}
-		print_frame_with_rw_bit(reference[i], frame, n_frames, "miss", fd); // frame 출력
-	}
-	print_result("ESC", page_fault, fd); // page fault 횟수 출력
+  // frame 초기화
+  for (int i = 0; i < n_frames; i++) {
+    frame[i].page = 0;
+    frame[i].r_bit = 0;
+    frame[i].w_bit = 0;
+  }
+  print_algorithm_start("ESC", n_frames, fd); // 알고리즘 정보 출력
+  for (int i = 0; i < REFERENCE_SIZE; i++) {
+    find_victim = 0;
+    is_hit = 0;
+    int j = 0;
+    for (j = 0; j < n_frames; j++) {
+      if (frame[j].page == 0)
+        break;
+      if (frame[j].page == reference[i].page) // hit!
+      {
+        is_hit = 1;
+        frame[j].r_bit = 1; // r_bit을 1로 설정
+        frame[j].w_bit = (reference[i].rw_bit == W_BIT);
+        break;
+      }
+    }
+    if (is_hit) // hit!
+    {
+      print_frame_with_rw_bit(reference[i], frame, n_frames, "HIT!",
+                              fd); // frame 출력
+      continue;                    // 다음 참조값으로 넘어감
+    }
+    ++page_fault;               // page fault 발생
+    if (page_fault <= n_frames) // 비어있는 프레임이 있으면
+    {
+      frame[j].page = reference[i].page;
+      frame[j].r_bit = 1;
+      frame[j].w_bit = (reference[i].rw_bit == W_BIT);
+    } else // frame이 가득 찬 경우
+    {
+      while (1) {
+        // 단계 1: (0, 0) 찾기
+        for (j = 0; j < n_frames; j++) {
+          if (frame[idx].r_bit == 0 && frame[idx].w_bit == 0) {
+            find_victim = 1;
+            break;
+          }
+          idx = (idx + 1) % n_frames; // 포인터를 다음 프레임으로 이동
+        }
+        // (0, 0)을 찾았다면 교체 후 반복문 탈출
+        if (find_victim) {
+          frame[idx].page = reference[i].page;
+          frame[idx].r_bit = 1;
+          frame[idx].w_bit = (reference[i].rw_bit == W_BIT);
+          break;
+        }
+        // 단계 2: (0, 1) 찾기
+        for (j = 0; j < n_frames; j++) {
+          if (frame[idx].r_bit == 0 && frame[idx].w_bit == 1) {
+            find_victim = 1;
+            break;
+          }
+          frame[idx].r_bit =
+              0; // 지나쳐가는 모든 프레임의 사용 비트를 0으로 설정
+          idx = (idx + 1) % n_frames;
+        }
+        // (0, 1)을 찾았다면
+        if (find_victim) {
+          frame[idx].page = reference[i].page;
+          frame[idx].r_bit = 1;
+          frame[idx].w_bit = (reference[i].rw_bit == W_BIT);
+          break;
+        }
+      }
+      idx = (idx + 1) % n_frames; // 포인터를 다음 프레임으로 이동
+    }
+    print_frame_with_rw_bit(reference[i], frame, n_frames, "miss",
+                            fd); // frame 출력
+  }
+  print_result("ESC", page_fault, fd); // page fault 횟수 출력
 }
--- project4/utils.c
+++ project4/utils.c
@@ -1,191 +1,185 @@
 #include "page_replacement.h"
 
 // 참조 문자열을 배열에 저장하는 함수
-void get_page_reference(int *page_reference, t_reference *reference)
-{
-	for (int i = 0; i < REFERENCE_SIZE; i++)
-		page_reference[i] = reference[i].page;
+void get_page_reference(int *page_reference, t_reference *reference) {
+  for (int i = 0; i < REFERENCE_SIZE; i++)
+    page_reference[i] = reference[i].page;
 }
 
 // 참조 문자열을 구조체 t_reference에 저장하는 함수
-void get_reference_with_rb_bit(int fd, t_reference *reference)
-{
-	int stdin;
+void get_reference_with_rb_bit(int fd, t_reference *reference) {
+  int stdin;
 
-	stdin = dup(STDIN_FILENO); // stdin을 복사
-	dup2(fd, STDIN_FILENO);	   // fd를 stdin으로 복사
-	for (int i = 0; i < REFERENCE_SIZE; i++)
-		scanf("%d(%c) ", &reference[i].page, &reference[i].rw_bit);
-	dup2(stdin, STDIN_FILENO); // stdin 복구
+  stdin = dup(STDIN_FILENO); // stdin을 복사
+  dup2(fd, STDIN_FILENO);    // fd를 stdin으로 복사
+  for (int i = 0; i < REFERENCE_SIZE; i++)
+    scanf("%d %c\nc", &reference[i].page, &reference[i].rw_bit);
+  dup2(stdin, STDIN_FILENO); // stdin 복구
 }
 
 // 에러 메시지 출력 후 프로그램을 종료하는 함수
-void exit_with_msg(char *msg)
-{
-	printf("%s\n", msg);
-	exit(1);
+void exit_with_msg(char *msg) {
+  printf("%s\n", msg);
+  exit(1);
 }
 
 // 알고리즘 정보를 출력하는 함수
-void print_algorithm_start(char *algorithm_type, int n_frames, int fd)
-{
-	char buf[100] = {0};
+void print_algorithm_start(char *algorithm_type, int n_frames, int fd) {
+  char buf[100] = {0};
 
-	printf("\033[0;32m=============================%s algorithm=============================\n\033[0m", algorithm_type);
-	dprintf(fd, "=============================%s algorithm=============================\n", algorithm_type);
-	printf("%10s", "Page");
-	dprintf(fd, "%10s", "Page");
-	for (int i = 0; i < n_frames; i++)
-	{
-		sprintf(buf, "Frame[%d]", i);
-		printf("%10s", buf);
-		dprintf(fd, "%10s", buf);
-	}
-	printf("%10s\n", "Result");
-	dprintf(fd, "%10s\n", "Result");
+  printf("\033[0;32m=============================%s "
+         "algorithm=============================\n\033[0m",
+         algorithm_type);
+  dprintf(fd,
+          "=============================%s "
+          "algorithm=============================\n",
+          algorithm_type);
+  printf("%10s", "Page");
+  dprintf(fd, "%10s", "Page");
+  for (int i = 0; i < n_frames; i++) {
+    sprintf(buf, "Frame[%d]", i);
+    printf("%10s", buf);
+    dprintf(fd, "%10s", buf);
+  }
+  printf("%10s\n", "Result");
+  dprintf(fd, "%10s\n", "Result");
 }
 
 // 알고리즘 정보를 출력하는 함수 - 연결리스트
-void print_algorithm_start_list(char *algorithm_type, int n_frames, int fd)
-{
-	printf("\033[0;32m=============================%s algorithm=============================\n\033[0m", algorithm_type);
-	dprintf(fd, "=============================%s algorithm=============================\n", algorithm_type);
-	printf("%10s", "Page");
-	dprintf(fd, "%10s", "Page");
-	printf("%10s", "Front");
-	dprintf(fd, "%10s", "Front");
-	for (int i = 1; i < n_frames - 1; i++)
-	{
-		printf("%10s", "->");
-		dprintf(fd, "%10s", "->");
-	}
-	printf("%10s", "Rear");
-	dprintf(fd, "%10s", "Rear");
-	printf("%10s\n", "Result");
-	dprintf(fd, "%10s\n", "Result");
+void print_algorithm_start_list(char *algorithm_type, int n_frames, int fd) {
+  printf("\033[0;32m=============================%s "
+         "algorithm=============================\n\033[0m",
+         algorithm_type);
+  dprintf(fd,
+          "=============================%s "
+          "algorithm=============================\n",
+          algorithm_type);
+  printf("%10s", "Page");
+  dprintf(fd, "%10s", "Page");
+  printf("%10s", "Front");
+  dprintf(fd, "%10s", "Front");
+  for (int i = 1; i < n_frames - 1; i++) {
+    printf("%10s", "->");
+    dprintf(fd, "%10s", "->");
+  }
+  printf("%10s", "Rear");
+  dprintf(fd, "%10s", "Rear");
+  printf("%10s\n", "Result");
+  dprintf(fd, "%10s\n", "Result");
 }
 
 // 프레임 배열을 출력하는 함수
-void print_frame_array(int page, int *frame, int n_frames, char *result, int fd)
-{
-	// 참조값 출력
-	printf("%10d", page);
-	// 참조값 파일에 저장
-	dprintf(fd, "%10d", page);
-	for (int i = 0; i < n_frames; i++)
-	{
-		// 프레임값 출력
-		printf("%10d", frame[i]);
-		// 프레임값 파일에 저장
-		dprintf(fd, "%10d", frame[i]);
-	}
-	printf("%10s\n", result);	   // 결과 (HIT, miss) 출력
-	dprintf(fd, "%10s\n", result); // 결과 (HIT, miss) 파일에 저장
+void print_frame_array(int page, int *frame, int n_frames, char *result,
+                       int fd) {
+  // 참조값 출력
+  printf("%10d", page);
+  // 참조값 파일에 저장
+  dprintf(fd, "%10d", page);
+  for (int i = 0; i < n_frames; i++) {
+    // 프레임값 출력
+    printf("%10d", frame[i]);
+    // 프레임값 파일에 저장
+    dprintf(fd, "%10d", frame[i]);
+  }
+  printf("%10s\n", result);      // 결과 (HIT, miss) 출력
+  dprintf(fd, "%10s\n", result); // 결과 (HIT, miss) 파일에 저장
 }
 
 // 프레임 연결 리스트를 출력하는 함수
-void print_frame_list(int page, t_frame *head, int n_frames, char *result, int fd)
-{
-	t_frame *tmp = head;
-	int cnt = 0;
+void print_frame_list(int page, t_frame *head, int n_frames, char *result,
+                      int fd) {
+  t_frame *tmp = head;
+  int cnt = 0;
 
-	// 참조값 출력
-	printf("%10d", page);
-	// 참조값 파일에 저장
-	dprintf(fd, "%10d", page);
-	while (tmp != NULL)
-	{
-		// 프레임값 출력
-		printf("%10d", tmp->page);
-		// 프레임값 파일에 저장
-		dprintf(fd, "%10d", tmp->page);
-		tmp = tmp->next;
-		++cnt;
-	}
-	while (cnt < n_frames)
-	{
-		printf("%10d", 0);
-		dprintf(fd, "%10d", 0);
-		++cnt;
-	}
-	printf("%10s\n", result);
-	dprintf(fd, "%10s\n", result);
+  // 참조값 출력
+  printf("%10d", page);
+  // 참조값 파일에 저장
+  dprintf(fd, "%10d", page);
+  while (tmp != NULL) {
+    // 프레임값 출력
+    printf("%10d", tmp->page);
+    // 프레임값 파일에 저장
+    dprintf(fd, "%10d", tmp->page);
+    tmp = tmp->next;
+    ++cnt;
+  }
+  while (cnt < n_frames) {
+    printf("%10d", 0);
+    dprintf(fd, "%10d", 0);
+    ++cnt;
+  }
+  printf("%10s\n", result);
+  dprintf(fd, "%10s\n", result);
 }
 
 // r_bit가 있는 프레임 배열을 출력하는 함수
-void print_frame_with_r_bit(int page, t_frame_with_r_bit *frame, int n_frames, char *result, int fd)
-{
-	// 참조값 출력
-	printf("%10d", page);
-	// 참조값 파일에 저장
-	dprintf(fd, "%10d", page);
-	for (int i = 0; i < n_frames; i++)
-	{
-		// 프레임값과 r_bit 출력
-		printf("%7d(%d)", frame[i].page, frame[i].r_bit);
-		// 프레임값과 r_bit 파일에 저장
-		dprintf(fd, "%7d(%d)", frame[i].page, frame[i].r_bit);
-	}
-	printf("%10s\n", result);	   // 결과 (HIT, miss) 출력
-	dprintf(fd, "%10s\n", result); // 결과 (HIT, miss) 파일에 저장
+void print_frame_with_r_bit(int page, t_frame_with_r_bit *frame, int n_frames,
+                            char *result, int fd) {
+  // 참조값 출력
+  printf("%10d", page);
+  // 참조값 파일에 저장
+  dprintf(fd, "%10d", page);
+  for (int i = 0; i < n_frames; i++) {
+    // 프레임값과 r_bit 출력
+    printf("%7d(%d)", frame[i].page, frame[i].r_bit);
+    // 프레임값과 r_bit 파일에 저장
+    dprintf(fd, "%7d(%d)", frame[i].page, frame[i].r_bit);
+  }
+  printf("%10s\n", result);      // 결과 (HIT, miss) 출력
+  dprintf(fd, "%10s\n", result); // 결과 (HIT, miss) 파일에 저장
 }
 
 // rw 비트가 있는 프레임 구조체 배열을 출력하는 함수
-void print_frame_with_rw_bit(t_reference reference, t_frame_with_rw_bit *frame, int n_frames, char *result, int fd)
-{
-	// 참조값 출력
-	printf("%7d(%c)", reference.page, reference.rw_bit);
-	// 참조값 파일에 저장
-	dprintf(fd, "%7d(%c)", reference.page, reference.rw_bit);
-	for (int i = 0; i < n_frames; i++)
-	{
-		// 프레임값과 r_bit 출력
-		printf("%4d(%d, %d)", frame[i].page, frame[i].r_bit, frame[i].w_bit);
-		// 프레임값과 r_bit 파일에 저장
-		dprintf(fd, "%4d(%d, %d)", frame[i].page, frame[i].r_bit, frame[i].w_bit);
-	}
-	printf("%10s\n", result);	   // 결과 (HIT, miss) 출력
-	dprintf(fd, "%10s\n", result); // 결과 (HIT, miss) 파일에 저장
+void print_frame_with_rw_bit(t_reference reference, t_frame_with_rw_bit *frame,
+                             int n_frames, char *result, int fd) {
+  // 참조값 출력
+  printf("%7d(%c)", reference.page, reference.rw_bit);
+  // 참조값 파일에 저장
+  dprintf(fd, "%7d(%c)", reference.page, reference.rw_bit);
+  for (int i = 0; i < n_frames; i++) {
+    // 프레임값과 r_bit 출력
+    printf("%4d(%d, %d)", frame[i].page, frame[i].r_bit, frame[i].w_bit);
+    // 프레임값과 r_bit 파일에 저장
+    dprintf(fd, "%4d(%d, %d)", frame[i].page, frame[i].r_bit, frame[i].w_bit);
+  }
+  printf("%10s\n", result);      // 결과 (HIT, miss) 출력
+  dprintf(fd, "%10s\n", result); // 결과 (HIT, miss) 파일에 저장
 }
 
 // page fault 횟수를 출력하는 함수
-void print_result(char *algorithm_type, int page_fault, int fd)
-{
-	float page_fault_rate = page_fault > 0 ? page_fault / (float)REFERENCE_SIZE * 100 : 0;
-	printf("\033[0;31m%s page fault: %d\n\033[0m", algorithm_type, page_fault);
-	dprintf(fd, "%s page fault: %d\n", algorithm_type, page_fault);
-	printf("\033[0;31m%s page fault rate: %.2f%%\n\033[0m", algorithm_type, page_fault_rate);
-	dprintf(fd, "%s page fault rate: %.2f%%\n", algorithm_type, page_fault_rate);
+void print_result(char *algorithm_type, int page_fault, int fd) {
+  float page_fault_rate =
+      page_fault > 0 ? page_fault / (float)REFERENCE_SIZE * 100 : 0;
+  printf("\033[0;31m%s page fault: %d\n\033[0m", algorithm_type, page_fault);
+  dprintf(fd, "%s page fault: %d\n", algorithm_type, page_fault);
+  printf("\033[0;31m%s page fault rate: %.2f%%\n\033[0m", algorithm_type,
+         page_fault_rate);
+  dprintf(fd, "%s page fault rate: %.2f%%\n", algorithm_type, page_fault_rate);
 }
 
 // 프레임 배열을 초기화하는 함수
-void init_frame(int *frame, int n_frames)
-{
-	for (int i = 0; i < n_frames; i++)
-		frame[i] = 0;
+void init_frame(int *frame, int n_frames) {
+  for (int i = 0; i < n_frames; i++)
+    frame[i] = 0;
 }
 
 // 프레임 연결 리스트 할당 해제하는 함수
-void free_frame_list(t_frame *head)
-{
-	t_frame *tmp;
+void free_frame_list(t_frame *head) {
+  t_frame *tmp;
 
-	while (head->next != NULL)
-	{
-		tmp = head;
-		head = head->next;
-		free(tmp);
-	}
-	free(head);
+  while (head->next != NULL) {
+    tmp = head;
+    head = head->next;
+    free(tmp);
+  }
+  free(head);
 }
 
 // 프레임 배열에서 hit인지 miss인지 확인하는 함수
-char is_hit(int *frame, int n_frames, int page)
-{
-	for (int i = 0; i < n_frames; i++)
-	{
-		if (frame[i] == page)
-			return 1;
-	}
-	return 0;
+char is_hit(int *frame, int n_frames, int page) {
+  for (int i = 0; i < n_frames; i++) {
+    if (frame[i] == page)
+      return 1;
+  }
+  return 0;
 }
